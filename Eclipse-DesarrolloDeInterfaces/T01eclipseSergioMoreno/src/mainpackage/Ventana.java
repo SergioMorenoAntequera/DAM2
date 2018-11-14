e6d277c29bea13f34985d03550a221903cf39fb9
Seran <seranmoreno500@gmail.com>
2018-11-15 00:26:03 +0100

    14/11/2018 Fin Casa
    asd

--- Eclipse-DesarrolloDeInterfaces/T01eclipseSergioMoreno/src/mainpackage/Ventana.java
+++ Eclipse-DesarrolloDeInterfaces/T01eclipseSergioMoreno/src/mainpackage/Ventana.java
@@ -1,25 +1,42 @@
 package mainpackage;
+import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.Image;
+import java.awt.Rectangle;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.KeyStroke;
 
 public class Ventana extends JFrame {
 	private static final long serialVersionUID = 1L;
 	Container lienzo = this.getContentPane();
 	
-	 private JMenu mArchivo, mReservas, mAyuda;
-	 private JMenuItem arcSalir, resAlta, resBaja, ayuAcerca;
+	private JMenuBar menuBar;
+	private JMenu mArchivo, mReservas, mAyuda;
+	private JMenuItem arcSalir, resAlta, resBaja, ayuAcerca;
+	private JButton bReservar;
 	
 	public Ventana() {
-		iniciarComponentes();
 		iniciarVentana();
+		iniciarComponentes();
 	}
 	
 	public void iniciarComponentes() {
+		//Primero creamos las barras
+		menuBar = new JMenuBar();
 		mArchivo = new JMenu("Archivo");
 			arcSalir = new JMenuItem("Salir");
 		mReservas = new JMenu("Reservar");
@@ -27,7 +44,52 @@ public class Ventana extends JFrame {
 			resBaja = new JMenuItem("Baja Reservas");
 		mAyuda = new JMenu("Ayuda");
 			ayuAcerca = new JMenuItem("Acerca de...");
-			
+		//Añadimos las barras
+		menuBar.add(mArchivo);
+		mArchivo.add(arcSalir);
+		arcSalir.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		menuBar.add(mReservas);
+		mReservas.setMnemonic('F');
+		
+		mReservas.add(resAlta);
+
+		resAlta.setAccelerator(KeyStroke.getKeyStroke('A', Toolkit.getDefaultToolkit ().getMenuShortcutKeyMask()));
+		resAlta.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		mReservas.add(resBaja);
+		resBaja.setAccelerator(KeyStroke.getKeyStroke('B', Toolkit.getDefaultToolkit ().getMenuShortcutKeyMask()));
+		resBaja.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "Función en desarrollo");
+			}
+		});
+		menuBar.add(mAyuda);
+		mAyuda.add(ayuAcerca);
+		ayuAcerca.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "Aplicacion la cual simulará una pequeña reserva en un hotel");
+			}
+		});
+		this.add(menuBar, BorderLayout.NORTH);
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(new FlowLayout());
+		bReservar = new JButton("REALIZAR RESERVA");
+		bReservar.setFont(new Font("Calibri", Font.BOLD , 40));
+		panel.add(bReservar);
+		this.add(panel);
+		
 	}
 	
 	public void iniciarVentana() {
