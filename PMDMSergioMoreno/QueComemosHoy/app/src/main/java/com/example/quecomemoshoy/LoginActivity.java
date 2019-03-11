e89c1e9918590f5d5f2c63f7ce172bdb1eb90642
Sergio <seranmoreno500@gmail.com>
2019-03-11 12:19:00 +0100

    Fin Clase 11/03/2019

--- PMDMSergioMoreno/QueComemosHoy/app/src/main/java/com/example/quecomemoshoy/LoginActivity.java
+++ PMDMSergioMoreno/QueComemosHoy/app/src/main/java/com/example/quecomemoshoy/LoginActivity.java
@@ -5,7 +5,13 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.quecomemoshoy.Objetos.FirebaseReferences;
+import com.example.quecomemoshoy.Objetos.Usuario;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -13,77 +19,82 @@ import com.google.firebase.auth.ActionCodeSettings;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private FirebaseAuth firebaseAuth;
-    private FirebaseAuth.AuthStateListener firebaseAuthListener;
+    private FirebaseAuth mAuth;
+    EditText etEmail, etPass;
+    Button bRegistrase, bLogin;
 
+    FirebaseDatabase database;
+    DatabaseReference usuariosRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        ActionCodeSettings actionCodeSettings =
-                ActionCodeSettings.newBuilder()
-                        // URL you want to redirect back to. The domain (www.example.com) for this
-                        // URL must be whitelisted in the Firebase Console.
-                        .setUrl("https://www.example.com/finishSignUp?cartId=1234")
-                        // This must be true
-                        .setHandleCodeInApp(true)
-                        .setIOSBundleId("com.example.ios")
-                        .setAndroidPackageName(
-                                "com.example.android",
-                                true, /* installIfNotAvailable */
-                                "12"    /* minimumVersion */)
-                        .build();
+        initThings();
 
-        //------------------------------------------------------------------------------------------
+    }
 
-        /*FirebaseAuth auth = FirebaseAuth.getInstance();
-        auth.sendSignInLinkToEmail(email, actionCodeSettings)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()) {
-                            Log.d("TAG", "Email sent.");
-                        }
-                    }
-                });*/
+    private void initThings() {
+        etEmail = findViewById(R.id.etEmail);
+        etPass = findViewById(R.id.etPass);
+        bRegistrase = findViewById(R.id.bRegistrarse);
+        bLogin = findViewById(R.id.bLogin);
 
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        usuariosRef = database.getReference(FirebaseReferences.USUARIOS_REFERENCE);
+    }
 
+    //----------------------------------------------------------------------------------------------
+    // Comprbamos si el usuario ha accedido anteriormente para un login automatico -----------------
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
 
-        //------------------------------------------------------------------------------------------
+    }
 
-        /*FirebaseAuth auth = FirebaseAuth.getInstance();
-        Intent intent = getIntent();
-        String emailLink = intent.getData().toString();
+    //----------------------------------------------------------------------------------------------
+    // Metodos variados ----------------------------------------------------------------------------
+    public void crearUsuario(View v) {
+        final String email = etEmail.getText().toString();
+        final String pass = etPass.getText().toString();
 
-        // Confirm the link is a sign-in with email link.
-        if (auth.isSignInWithEmailLink(emailLink)) {
-            // Retrieve this from wherever you stored it
-            String email = "someemail@domain.com";
-
-            // The client SDK will parse the code from the link for you.
-            auth.signInWithEmailLink(email, emailLink)
-                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+        if (pass.length() < 6) {
+            Toast.makeText(getApplicationContext(), "La contraseña debe tener minimo 6 caracteres", Toast.LENGTH_SHORT).show();
+        } else {
+            mAuth.createUserWithEmailAndPassword(email, pass)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
-                                Log.d("TAG", "Successfully signed in with email link!");
-                                AuthResult result = task.getResult();
-                                // You can access the new user via result.getUser()
-                                // Additional user info profile *not* available via:
-                                // result.getAdditionalUserInfo().getProfile() == null
-                                // You can check if the user is new or existing:
-                                // result.getAdditionalUserInfo().isNewUser()
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d("TAGGGGG", "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+
+                                Toast.makeText(getApplicationContext(), "Usuario registrado exitosamente", Toast.LENGTH_SHORT).show();
+                                String[] aux = email.split("@");
+                                usuariosRef.child(user.getUid()).setValue(new Usuario(email, pass));
+
                             } else {
-                                Log.e("TAG", "Error signing in with email link", task.getException());
+                                // If sign in fails, display a message to the user.
+                                Log.w("TAGGGGG", "createUserWithEmail:failure", task.getException());
+                                if(task.getException().toString().contains("The email address is badly formatted.")){
+                                    Toast.makeText(getApplicationContext(), "Error al introducir el correo.", Toast.LENGTH_SHORT).show();
+                                }
+                                if(task.getException().toString().contains("The email address is already in use by another account.")){
+                                    Toast.makeText(getApplicationContext(), "Correo ya registrado.", Toast.LENGTH_SHORT).show();
+                                }
                             }
                         }
                     });
-        }*/
-
+        }
     }
 }
